b9c26ffcaf619512a273118a9fb2af803c31e986
Tossapon Nuanchuay <tossapon.nuanchuay@agoda.com>
2017-10-29 02:38:19 +0700

    add functionality to be able to run natural go command e.g. gorun test -v *.go, It means we can run test in main package. Pray for windows.

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
@@ -9,19 +11,34 @@ import (
 )
 
 func main() {
-	all, err := filepath.Glob("*.go")
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) == 0 {
+		args = append(args, []string{"help"}...)
+	}
+
+	all, err := filepath.Glob(args[len(args)-1])
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	args = args[:len(args)-1]
+
 	gofiles := make([]string, 0, len(all))
-	for i := range all {
-		if !strings.HasSuffix(all[i], "_test.go") {
-			gofiles = append(gofiles, all[i])
+	for _, filename := range all {
+		if args[0] == "run" {
+			if !strings.HasSuffix(filename, "_test.go") {
+				gofiles = append(gofiles, filename)
+			}
+		} else {
+			gofiles = append(gofiles, filename)
 		}
 	}
 
-	args := append([]string{"run"}, gofiles...)
+	args = append(args, gofiles...)
+
+	fmt.Println(args)
 
 	cmd := exec.Command("go", args...)
 	cmd.Stdout = os.Stdout
